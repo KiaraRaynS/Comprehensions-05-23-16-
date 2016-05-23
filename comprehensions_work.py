bf43df180c6eae48ace1900f7e6fe4aa32f8efa9
KiaraRaynS <sherman.rayn@outlook.com>
2016-05-23 13:16:43 -0400

    Begin Project

---
+++ comprehensions_work.py
@@ -0,0 +1,9 @@
+
+
+def remove_vowels():
+    letter_list = list("List Comprehensions are the Greatest!".lower())
+    end_list = [for letter in letter_list if letter != ('a', 'e', 'i', 'o', 'u')]
+    print(end_list)
+
+
+def
