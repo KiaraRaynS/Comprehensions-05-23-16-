d4f90d6e31f734999d9b7782819a6df2134af642
KiaraRaynS <sherman.rayn@outlook.com>
2016-05-23 17:04:03 -0400

    Bug fixes

--- functions_for_comprehensions.py
+++ functions_for_comprehensions.py
@@ -13,30 +13,43 @@ def remove_vowels():
 
 
 def temp_string_to_float():
+    floated_temps = []
     water_data = read_water_data()
     water_temps = [item[4] for item in water_data]
-    water_temps = ' '.join(water_temps)
     for item in water_temps:
         item = float(item)
-    water_temps_float = [item for item in water_temps]
-    print(water_temps)
+        floated_temps.append(item)
 
-temp_string_to_float()
+    print(floated_temps)
 
 
 def water_temperatures():
     water_data = read_water_data()
     water_temps = [item[4] for item in water_data]
-    fahrenheit_temperature = [temp for temp in water_temps]
+    floated_temps = []
+    for item in water_temps:
+        item = float(item)
+        floated_temps.append(item)
+    fahrenheit_temperature = [temp * 1.8 + 32 for temp in floated_temps]
     print(fahrenheit_temperature)
-
-
-water_temperatures()
 
 
 def water_dates_height_dictionairy():
     water_data = read_water_data()
+    dates_list = [item[5] for item in water_data]
     dates = []
-    height = []
-    dates_height_dict = {}
+    for date in dates_list:
+        date = date.replace('\n', '')
+        dates.append(date)
+    height = [item[1] for item in water_data]
+    dates_height_dict = {date: height[index] for index, date in enumerate(dates)}
+    print(dates_height_dict)
+
+
+def average_grades():
+    complete_dict = {'Gale': {'Homework 1': 88, 'Homework 2': 76},
+                        'Jordan': {'Homework 1': 92, 'Homework 2': 87},
+                        'Peyton': {'Homework 1': 84, 'Homework 2': 77},
+                        'River': {'Homework 1': 85, 'Homework 2': 91}
+    }
 
