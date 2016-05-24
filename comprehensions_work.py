92e9ab7fda2e0376925aeb22a95f6be5805ccad2
KiaraRaynS <sherman.rayn@outlook.com>
2016-05-24 14:41:28 -0400

    Fixed homework averages

--- comprehensions_work.py
+++ comprehensions_work.py
@@ -2,8 +2,6 @@ from functions_for_comprehensions import remove_vowels
 from functions_for_comprehensions import temp_string_to_float
 from functions_for_comprehensions import water_temperatures
 from functions_for_comprehensions import water_dates_height_dictionairy
-from functions_for_comprehensions import get_day_of_week
-from functions_for_comprehensions import average_height
 from functions_for_comprehensions import nested_comprehension
 
 print('"List Comprehensions are the Greatest!" Now without Vowels.')
@@ -17,9 +15,6 @@ water_temperatures()
 
 print("Dictionairy of wave heights by dates: ")
 water_dates_height_dictionairy()
-
-print("Average wave height: ")
-average_height()
 
 print("Average Homework 1 grades")
 nested_comprehension()
--- functions_for_comprehensions.py
+++ functions_for_comprehensions.py
@@ -1,4 +1,3 @@
-from enum import Enum
 
 
 def read_water_data():
@@ -47,64 +46,24 @@ def water_dates_height_dictionairy():
     print(dates_height_dict)
 
 
-def get_day_of_week(year, month, day):
-    from enum import Enum
-    class DayOfWeek(Enum):
-        Sunday = 0
-        Monday = 1
-        Tuesday = 2
-        Wednesday = 3
-        Thursday = 4
-        Friday = 5
-        Saturday = 6
-    month_table = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
-    year -= 1 if month < 3 else 0
-    return DayOfWeek(int((year + year / 4 - year / 100 + year / 400 + month_table[month - 1] + day) % 7))
-
-
-def average_height():
+def average_wave_by_day():
     water_data = read_water_data()
     dates_list = [item[5] for item in water_data]
     heights_list = [item[1] for item in water_data]
-    day_list = [item[8:10] for item in dates_list]
-    days = []
-    for day in day_list:
-        day = int(day)
-        days.append(day)
-    days_heights = {day: heights_list[index] for index, day in enumerate(days)}
-    for day in days_heights.keys():
-        monday_heights = []
-        tuesday_heights = []
-        wednesday_heights = []
-        thursday_heights = []
-        friday_heights = []
-        saturday_heights = []
-        sunday_heights = []
-        years = 2015
-        months = 8
-        thanks_joel = get_day_of_week(years, months, day)
-        if thanks_joel == DayOfWeek.Monday:
-            monday_heights.append(days)
-        if thanks_joel == DayOfWeek.Tuesday:
-            tuesday_heights.append(days)
-        if thanks_joel == DayOfWeek.Wednesday:
-            wednesday_heights.append(days)
-        if thanks_joel == DayOfWeek.Thursday:
-            thursday_heights.append(days_heights.values())
-        if thanks_joel == DayOfWeek.Friday:
-            friday_heights.append(days_heights.values())
-        if thanks_joel == DayOfWeek.Saturday:
-            saturday_heights.append(days_heights.values())
-        if thanks_joel == DayOfWeek.Sunday:
-            sunday_heights.append(days_heights.values())
-    print(sunday_heights)
+    dates_heights = {date: heights_list[index] for index, date in enumerate(dates_list)}
+    print(dates_heights)
 
 
 def nested_comprehension():
+    from statistics import mean
+    homework_1 = []
     homework_dict = {'Gale': {'Homework 1': 88, 'Homework 2': 76},
                      'Jordan': {'Homework 1': 92, 'Homework 2': 87},
                      'Peyton': {'Homework 1': 84, 'Homework 2': 77},
-                     'River': {'Homework 1': 85, 'Homework 2': 91}
+                     'River': {'Homework 1': 85, 'Homework 2': 91},
                      }
-    homework_1_grades = for key in homework_dict.keys():
-
+#    homework_grades = [value[1] for value in homework_dict.items()]
+    for key, value in homework_dict.items():
+        homework_1.append(value['Homework 1'])
+    average = sum(homework_1)/len(homework_1)
+    return(average)
